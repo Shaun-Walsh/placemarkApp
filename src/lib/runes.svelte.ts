6a34befdc1e6f3c8e08ca116bbf0ee75bcd8f7bc
Shaun Walsh <shaunwalsh24@gmail.com>
2025-05-24 18:06:49 +0100

    created palcemark service and connected to login api

--- src/lib/runes.svelte.ts
+++ src/lib/runes.svelte.ts
@@ -1,2 +1,8 @@
 export const subTitle = $state({ text: "" });
-export const loggedInUser = $state({ email: "" });
+export const loggedInUser = $state({ 
+    email: "",
+    name: "",
+    token: "",
+    _id: ""
+ });
+
--- src/routes/login/LoginForm.svelte
+++ src/routes/login/LoginForm.svelte
@@ -3,26 +3,34 @@
   import Message from "$lib/ui/Message.svelte";
   import UserCredentials from "$lib/ui/UserCredentials.svelte";
   import { loggedInUser } from "$lib/runes.svelte";
+  import { placemarkService } from "$lib/services/placemark-service";
 
   let email = $state("");
   let password = $state("");
   let message = $state("");
 
   async function login() {
-    const success = true;
-    if (success) {
+    console.log(`attempting to log in email: ${email} with password: ${password}`);
+    let session = await placemarkService.login(email, password);
+    if (session) {
       loggedInUser.email = email;
-      goto("/add"); 
+      loggedInUser.name = session.name;
+      loggedInUser.token = session.token;
+      loggedInUser._id = session._id;
+      console.log(`Session: ${JSON.stringify(session)}`);
+      goto("/add");
     } else {
-      message = "Error Trying to sign up";
+      email = "";
+      password = "";
+      message = "Invalid Credentials";
     }
   }
 </script>
 
-<div class="box">
-  {#if message}
-    <Message {message} />
-  {/if}
+{#if message}
+  <Message {message} />
+{/if}
+<form on:submit|preventDefault={login}>
   <UserCredentials bind:email bind:password />
-  <button onclick={() => login()} class="button">Log In</button>
-</div>
+  <button class="button is-success is-fullwidth">Log In</button>
+</form>
