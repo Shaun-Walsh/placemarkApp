2b50540b77e8861132bcc09f4d393a6d586308f4
Shaun Walsh <shaunwalsh24@gmail.com>
2025-05-26 12:51:22 +0100

    persistance through local sotrage implemented and add venue view updated

--- src/routes/add/+page.svelte
+++ src/routes/add/+page.svelte
@@ -2,19 +2,33 @@
   import { subTitle, loggedInUser } from "$lib/runes.svelte";
   import { placemarkService } from "$lib/services/placemark-service";
   import { onMount } from "svelte";
-  import type { VenueType } from "$lib/types/placemark-types";
+  import type { VenueType, Venue } from "$lib/types/placemark-types";
   import AddVenueForm from "./AddVenueForm.svelte";
   import Card from "$lib/ui/Card.svelte";
+  import VenueList from "$lib/ui/VenueList.svelte";
 
   subTitle.text = "Add a Venue to Placemark";
 
   let venueTypesList: VenueType[] = [];
+  let venues: Venue[] = [];
   onMount(async () => {
     try {
       venueTypesList = await placemarkService.getVenueTypes(loggedInUser.token);
+      venues = await placemarkService.getVenues(loggedInUser.token);
     } catch (error) {
-      console.error("Failed to fetch venue types:", error);
+      console.error("Failed to fetch data:", error);
     }
   });
 </script>
-<Card title= "Add Venue"><AddVenueForm {venueTypesList} /></Card>
+<div class="columns">
+  <div class="column">
+    <Card title="Venues added to Date">
+      <VenueList {venues} />
+    </Card>
+  </div>
+  <div class="column">
+    <Card title="Please add a Venue">
+      <AddVenueForm {venueTypesList} />
+    </Card>
+  </div>
+</div>
--- src/routes/logout/+page.svelte
+++ src/routes/logout/+page.svelte
@@ -6,5 +6,6 @@
   loggedInUser.name = "";
   loggedInUser.token = "";
   loggedInUser._id = "";
+  localStorage.removeItem("placemark");
   goto("/");
 </script>
